f298824039395b0af63ae0e74c10c3df6647f659
Early Ehlinger <earlye@gmail.com>
2016-12-08 16:38:58 -0600

    Bugfix

--- src/main/java/com/github/mstarodubtsev/dropwizard/MDCContext.java
+++ src/main/java/com/github/mstarodubtsev/dropwizard/MDCContext.java
@@ -4,12 +4,13 @@ import org.apache.log4j.MDC;
 
 public class MDCContext implements AutoCloseable {
 
-    String key;
-    
+    private final String key;
+
     public MDCContext(String key, String value) {
+        this.key = key;
         MDC.put(key, value);
     }
-    
+
     @Override
     public void close() throws Exception {
         MDC.remove(key);
